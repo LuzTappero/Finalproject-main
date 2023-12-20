a15c7935c22a0a3108124bbf36816ee50edf1d83
Luz.Tappero <luztappero@gmail.com>
2023-12-20 18:16:27 -0300

    20/12 primero

--- FinalProject/Home/urls.py
+++ FinalProject/Home/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.Home, name='Home'),
     path('registro/', views.registro_view, name='registro'), #type:ignore
-    path('login/', views.login_view, name='login')
-    # path('logout', LogoutView.as_view(template_name='Home/logout.html'), name='logout')
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(template_name='Home/logout.html'), name='logout')
 ]
